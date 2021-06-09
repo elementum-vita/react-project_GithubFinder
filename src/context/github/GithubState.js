315583584c8b845361f2184256de497031f0831a
elementum-vita <82609134+elementum-vita@users.noreply.github.com>
2021-06-09 17:44:16 -0400

    Add files via upload
    update from work on 09-June-2021

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,6 +10,17 @@ import {
     GET_REPOS
 } from '../types';
 
+let githubClientID;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+  githubClientID = process.env.REACT_APP_GITHUB_CLIENT_ID
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET
+} else {
+  githubClientID = process.env.GITHUB_CLIENT_ID
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET
+}
+
 // function to initiate state, include all our actions
 const GithubState = props => {
     // global state for anything that has to do with github 
@@ -28,9 +39,7 @@ const GithubState = props => {
         setLoading();
     
         const res = await axios.get(
-          `https://api.github.com/search/users?q=${text}&client_id=${
-            process.env.REACT_APP_GITHUB_CLIENT_ID
-          }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+          `https://api.github.com/search/users?q=${text}&client_id=${githubClientID}&client_secret=${githubClientSecret}`
         );
             
         dispatch({
@@ -44,9 +53,7 @@ const GithubState = props => {
         setLoading();
     
         const res = await axios.get(
-          `https://api.github.com/users/${username}?client_id=${
-            process.env.REACT_APP_GITHUB_CLIENT_ID
-          }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+          `https://api.github.com/users/${username}?client_id=${githubClientID}&client_secret=${githubClientSecret}`
         );
            
         dispatch({
@@ -60,9 +67,7 @@ const GithubState = props => {
       setLoading(true);
 
       const res = await axios.get(
-        `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
-          process.env.REACT_APP_GITHUB_CLIENT_ID
-        }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientID}&client_secret=${githubClientSecret}`
       );
        
       dispatch({
